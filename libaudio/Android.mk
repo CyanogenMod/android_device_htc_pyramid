d571c17899c7959152abae8985463700491c96a6
cretin45 <cretin45@gmail.com>
2011-07-27 13:26:20 -0700

    Add libaudio-msm8660 from CAF.

---
+++ libaudio/Android.mk
@@ -0,0 +1,62 @@
+
+ifneq ($(BUILD_TINY_ANDROID),true)
+
+LOCAL_PATH := $(call my-dir)
+
+# ---------------------------------------------------------------------------------
+#             Make the Shared library libaudiopolicy
+# ---------------------------------------------------------------------------------
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := AudioPolicyManager.cpp
+LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_SHARED_LIBRARIES += libutils
+LOCAL_SHARED_LIBRARIES += libmedia
+LOCAL_MODULE := libaudiopolicy
+
+LOCAL_STATIC_LIBRARIES := libaudiopolicybase
+
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+  LOCAL_CFLAGS += -DWITH_A2DP
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+
+# ---------------------------------------------------------------------------------
+#             Make the Shared library libaudio
+# ---------------------------------------------------------------------------------
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libaudio
+LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_SHARED_LIBRARIES += libutils
+LOCAL_SHARED_LIBRARIES += libmedia
+LOCAL_SHARED_LIBRARIES += libhardware_legacy
+LOCAL_SHARED_LIBRARIES += libaudioalsa
+LOCAL_SHARED_LIBRARIES += libacdbloader
+LOCAL_SHARED_LIBRARIES += libacdbmapper
+
+ifeq ($TARGET_OS)-$(TARGET_SIMULATOR),linux-true)
+LOCAL_LDLIBS += -ldl
+endif
+ifneq ($(TARGET_SIMULATOR),true)
+LOCAL_SHARED_LIBRARIES += libdl
+endif
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+LOCAL_SHARED_LIBRARIES += liba2dp libbinder
+endif
+
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audio-alsa
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audcal
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audio-acdb-util
+LOCAL_SRC_FILES += AudioHardware.cpp
+LOCAL_CFLAGS += -fno-short-enums
+LOCAL_CFLAGS += -DQC_PROP
+LOCAL_STATIC_LIBRARIES += libaudiointerface
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif # not BUILD_TINY_ANDROID
+
