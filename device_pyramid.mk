4d3e4bdd4b93fd89729ef4692af30ddfd5da95b8
Keyan Mobli <persiansown@gmail.com>
2011-09-11 00:23:26 -0500

    Add wifi calling

--- device_pyramid.mk
+++ device_pyramid.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/htc/pyramid/modules/bcm4329.ko:system/lib/modules/bcm4329.ko
 
+# Wifi Calling
+PRODUCT_COPY_FILES += \
+    device/htc/pyramid/prebuilt/MS-HTCDP-KNT20-02-A0-GB.apk:/system/app/MS-HTCDP-KNT20-02-A0-GB.apk
+
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
