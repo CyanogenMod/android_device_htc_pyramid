73199d5b3bf9d21a601fd261b8439a733b5bc4c6
cretin45 <cretin45@gmail.com>
2012-03-27 13:05:50 -0700

    Fix up extract

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,6 @@ PRODUCT_COPY_FILES += \\
 	$OUTDIR/proprietary/lib/libacdbloader.so:obj/lib/libacdbloader.so \\
 	$OUTDIR/proprietary/lib/libacdbmapper.so:obj/lib/libacdbmapper.so \\
 	$OUTDIR/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \\
-	$OUTDIR/proprietary/lib/libril.so:obj/lib/libril.so \\
 	$OUTDIR/proprietary/lib/libv8.so:obj/lib/libv8.so
 
 PRODUCT_COPY_FILES += \\
