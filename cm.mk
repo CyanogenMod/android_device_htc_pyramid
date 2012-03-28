b693b2b76aca6d82f1872e0a3fe3dacf35c24805
Ethan Chen <intervigil@gmail.com>
2012-03-27 20:13:20 -0700

    pyramid: don't override BUILD_ID
    Change-Id: I8513a0b486bc54b195a3c7756eb5347d82e059b9

--- cm.mk
+++ cm.mk
@@ -4,7 +4,6 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := pyramid
 
-
 TARGET_BOOTANIMATION_NAME := vertical-540x960
 
 # Inherit some common CM stuff.
@@ -21,9 +20,4 @@ PRODUCT_MODEL := HTC Sensation
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_pyramid BUILD_ID=IML74K BUILD_FINGERPRINT=htc_europe/htc_pyramid/pyramid:4.0.3/IML74K/275847.101:user/release-keys PRIVATE_BUILD_DESC="3.25.401.101 CL275847 release-keys" BUILD_NUMBER=275847
-
-
-
-
-
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_pyramid BUILD_FINGERPRINT=htc_europe/htc_pyramid/pyramid:4.0.3/IML74K/275847.101:user/release-keys PRIVATE_BUILD_DESC="3.25.401.101 CL275847 release-keys" BUILD_NUMBER=275847
