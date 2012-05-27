e7d5c60545e8ca240085b5e211bea8b9e4c0493c
Ethan Chen <intervigil@gmail.com>
2012-05-27 12:43:39 -0700

    pyramid: use TMOUS ICS fingerprint
    Change-Id: Iecf3e5bc98f395bbddd5a0bfd1cc4c2aeaca48ba

--- cm.mk
+++ cm.mk
@@ -20,4 +20,4 @@ PRODUCT_MODEL := HTC Sensation
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_pyramid BUILD_FINGERPRINT=htc_europe/htc_pyramid/pyramid:4.0.3/IML74K/309896.3:user/release-keys PRIVATE_BUILD_DESC="3.32.401.3 CL309896 release-keys" BUILD_NUMBER=309896
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_pyramid BUILD_FINGERPRINT=tmous/htc_pyramid/pyramid:4.0.3/IML74K/356011.14:user/release-keys PRIVATE_BUILD_DESC="3.32.531.14 CL356011 release-keys" BUILD_NUMBER=356011
