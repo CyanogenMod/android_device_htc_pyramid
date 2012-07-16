113bce292e6ebb832622d688f715b0c508b775eb
cretin45 <cretin45@gmail.com>
2012-07-16 12:04:15 -0700

    Fix framework path issue.

--- device_pyramid.mk
+++ device_pyramid.mk
@@ -139,4 +139,4 @@ $(call inherit-product, device/htc/pyramid/media_a1026.mk)
 ## htc audio settings
 $(call inherit-product, device/htc/pyramid/media_htcaudio.mk)
 
-$(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
