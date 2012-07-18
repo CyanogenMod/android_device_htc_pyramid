e33eb20434b7264938c1b01ac75b648f675bf7e7
cretin45 <cretin45@gmail.com>
2012-07-18 10:48:08 -0700

    Fix liblights crash.

--- liblights/lights.c
+++ liblights/lights.c
@@ -307,7 +307,7 @@ static struct hw_module_methods_t lights_module_methods = {
 };
 
 
-const struct hw_module_t HAL_MODULE_INFO_SYM = {
+struct hw_module_t HAL_MODULE_INFO_SYM = {
 	.tag = HARDWARE_MODULE_TAG,
 	.version_major = 1,
 	.version_minor = 0,
