33a9b4a48b94fadb909a755d31fcf4b55bf23d3e
Ethan Chen <intervigil@gmail.com>
2012-07-19 00:00:43 -0700

    pyramid: re-organize init, change class late_start to main

--- device_pyramid.mk
+++ device_pyramid.mk
@@ -43,6 +43,7 @@ $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329
 
 # Boot ramdisk setup
 PRODUCT_COPY_FILES += \
+    device/htc/pyramid/ramdisk/fstab.pyramid:root/fstab.pyramid \
     device/htc/pyramid/ramdisk/init.pyramid.rc:root/init.pyramid.rc \
     device/htc/pyramid/ramdisk/init.pyramid.usb.rc:root/init.pyramid.usb.rc \
     device/htc/pyramid/ramdisk/ueventd.pyramid.rc:root/ueventd.pyramid.rc
