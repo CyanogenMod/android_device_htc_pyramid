2705f554d5010e308f4dae39dd21a1fdf52dbce6
cretin45 <cretin45@gmail.com>
2012-07-19 17:00:23 -0700

    Include libril with a removed patch that force a min verison of v6.
    Change-Id: Id33d361b2904460eb75c3cd152c9417e5aeaad78

---
+++ libril/Android.mk
@@ -0,0 +1,22 @@
+# Copyright 2006 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    ril.cpp \
+    ril_event.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libutils \
+    libbinder \
+    libcutils \
+    libhardware_legacy
+
+LOCAL_CFLAGS :=
+
+LOCAL_MODULE:= libril
+
+LOCAL_LDLIBS += -lpthread
+
+include $(BUILD_SHARED_LIBRARY)
