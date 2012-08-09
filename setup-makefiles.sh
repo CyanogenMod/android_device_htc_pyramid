65ac864766f8b08f521e3a34da596c59717e9ae6
Ethan Chen <intervigil@gmail.com>
2012-08-09 01:23:58 -0700

    pyramid: clean up device files
    Squash of the following commits:
    
    pyramid: sort proprietary files
    
    pyramid: remove unused flags
    
    pyramid: remove unparseable user/group here
    
    pyramid: remove unused libv8
    
    pyramid: remove unused recovery section

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,8 +24,7 @@ MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 PRODUCT_COPY_FILES += \\
 	$OUTDIR/proprietary/lib/libacdbloader.so:obj/lib/libacdbloader.so \\
 	$OUTDIR/proprietary/lib/libacdbmapper.so:obj/lib/libacdbmapper.so \\
-	$OUTDIR/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \\
-	$OUTDIR/proprietary/lib/libv8.so:obj/lib/libv8.so
+	$OUTDIR/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so
 
 PRODUCT_COPY_FILES += \\
 EOF
