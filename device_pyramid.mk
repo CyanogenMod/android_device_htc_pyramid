4d8ff5fea0894b4567da9bc93cdfc1169dcdf4c7
Ethan Chen <intervigil@gmail.com>
2012-12-03 01:02:16 -0800

    pyramid: use separate makefile to copy BT firmware

--- device_pyramid.mk
+++ device_pyramid.mk
@@ -94,6 +94,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
+# Bluetooth firmware
+$(call inherit-product, device/htc/msm8660-common/bcm_hcd.mk)
+
 ## misc
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.enable_bypass=1 \
